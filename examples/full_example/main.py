5d48a93743a7ae64a547f121bae86a3fa401c352
macbookair <kivydevelopment@gmail.com>
2020-11-02 09:07:16 +0300

    Added `Example` class

--- examples/full_example/main.py
+++ examples/full_example/main.py
@@ -1,12 +1,48 @@
-from kivy.uix.widget import Widget
-
 from kivymd.app import MDApp
 
-
-class MainApp(MDApp):
-    def build(self):
-        return Widget()
+from kivymd_extensions.filemanager import FileManager
 
 
-if __name__ == "__main__":
-    MainApp().run()
+class Example(MDApp):
+    def on_context_menu(self, instance_file_manager, name_context_plugin):
+        print("Event 'on_context_menu'", instance_file_manager, name_context_plugin)
+
+    def on_tap_file(self, instance_file_manager, path):
+        print("Event 'on_tap_file'", instance_file_manager, path)
+
+    def on_tap_dir(self, instance_file_manager, path):
+        print("Event 'on_tap_dir'", instance_file_manager, path)
+
+    def on_tab_switch(
+        self,
+        instance_file_manager,
+        instance_tabs,
+        instance_tab,
+        instance_tab_label,
+        tab_text,
+    ):
+        print(
+            "Event 'on_tab_switch'",
+            instance_file_manager,
+            instance_tabs,
+            instance_tab,
+            instance_tab_label,
+            tab_text,
+        )
+
+    def on_start(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Orange"
+        manager = FileManager(
+            path_to_skin=None,
+        )
+        manager.bind(
+            on_tap_file=self.on_tap_file,
+            on_tap_dir=self.on_tap_dir,
+            on_tab_switch=self.on_tab_switch,
+            on_context_menu=self.on_context_menu,
+        )
+        manager.open()
+
+
+Example().run()
