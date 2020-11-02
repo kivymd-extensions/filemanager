092f3beef67081501611ce12a3a40a79a923b8c0
macbookair <kivydevelopment@gmail.com>
2020-11-02 22:09:12 +0300

    Added module imports

--- kivymd_extensions/filemanager/__init__.py
+++ kivymd_extensions/filemanager/__init__.py
@@ -1,3 +1,10 @@
+from kivy.factory import Factory
+
+from kivymd_extensions.filemanager.libs import tools
 from kivymd_extensions.filemanager.filemanager import FileManager
 
+Factory.register(
+    "CustomFileChooserIcon",
+    module="kivymd_extensions.filemanager.file_chooser_icon",
+)
 __version__ = "0.1.0"
